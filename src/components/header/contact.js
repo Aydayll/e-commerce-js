2e4f41f63e837164b4d7eabe401ca1b3a1ca1f33
Aydayll <makenovaay23@gmail.com>
2020-12-03 16:04:13 +0600

    style the header top

--- src/components/header/contact.js
+++ src/components/header/contact.js
@@ -3,9 +3,9 @@ const Contact = () => {
     contactList.className ="header__contact"
     contactList.innerHTML= 
     `
-    <li class= "header__nav_li"><a href="">Chat With Us</a></li>
-    <li class= "header__nav_li"><a href="">+420 336 775 664</a></li>
-    <li class= "header__nav_li"><a href="">info@freshnesecom.com</a></li>
+    <li class= "header__nav_li"><a class= "header__contact_a"href="">Chat With Us</a></li>
+    <li class= "header__nav_li"><a class= "header__contact_a"href="">+420 336 775 664</a></li>
+    <li class= "header__nav_li"><a class= "header__contact_a"href="">info@freshnesecom.com</a></li>
     `;
     return contactList .outerHTML;
 };
--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -3,9 +3,9 @@ const Nav = () => {
     tagNav.className = "header__nav"
     tagNav.innerHTML= 
     `
-    <li class= "header__nav_li"><a href="">Blog</a></li>
-    <li class= "header__nav_li"><a href="">About Us</a></li>
-    <li class= "header__nav_li"><a href="">Careers</a></li>
+    <li class= "header__nav_li"><a class= "header__nav_a" href="">Blog</a></li>
+    <li class= "header__nav_li"><a class= "header__nav_a" href="">About Us</a></li>
+    <li class= "header__nav_li"><a class= "header__nav_a" href="">Careers</a></li>
     `;
     return tagNav .outerHTML;
 };
