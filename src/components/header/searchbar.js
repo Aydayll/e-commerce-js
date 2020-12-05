7219fe2e2599411862447deac4ce68629665f315
Aydayll <makenovaay23@gmail.com>
2020-12-06 04:01:40 +0600

    styling the searchbar and edit the searchbar  js

--- src/components/header/searchbar.js
+++ src/components/header/searchbar.js
@@ -1,10 +1,10 @@
 const searchBar = () => {
   const div = document.createElement('div');
 
-div.className="header__search-bar search-bar";
+div.className='header__search-bar search-bar';
 
 div.innerHTML =`
-<ul>
+<ul class="search-bar__list">
    <h3 class="search-bar__title">All categories</h3>
    <li class="search-bar__item"><a class="search-bar__link" href="#">Random Text</a></li>
    <li class="search-bar__item"><a class="search-bar__link" href="#">Random Text</a></li>
