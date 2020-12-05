6561ee20f2c4c4ffe6e11f50e7f40d57a22455a8
Aydayll <makenovaay23@gmail.com>
2020-12-06 03:01:32 +0600

    add the ul to system.js

--- src/components/header/system.js
+++ src/components/header/system.js
@@ -1,3 +1,9 @@
 const System = () => {
-    
-}
+    const div = document.createElement('div');
+    const ul = document.createElement('ul');
+
+ul.innerHTML = `
+<li><a href="#"></a></li>
+<li><a href="#"></a></li>
+`;
+};
