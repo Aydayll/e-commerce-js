a0ecba5484b8e97a2947f10ac97f09d8703e90c2
Aydayll <makenovaay23@gmail.com>
2020-12-06 02:11:35 +0600

    change the header__top and styles for it

--- src/components/header/contact.js
+++ src/components/header/contact.js
@@ -1,11 +1,11 @@
 const Contact = () => {
     const contactList = document.createElement('ul');
-    contactList.className ="header__contact"
+    contactList.className ="header__contact contact"
     contactList.innerHTML= 
     `
-    <li class= "header__nav_li"><a class= "header__contact_a"href="">Chat With Us</a></li>
-    <li class= "header__nav_li"><a class= "header__contact_a"href="">+420 336 775 664</a></li>
-    <li class= "header__nav_li"><a class= "header__contact_a"href="">info@freshnesecom.com</a></li>
+    <li class= "contact__item"><a class= "contact__link"href="">Chat With Us</a></li>
+    <li class= "contact__item"><a class= "contact__link"href="">+420 336 775 664</a></li>
+    <li class= "contact__item"><a class= "contact__link"href="">info@freshnesecom.com</a></li>
     `;
     return contactList .outerHTML;
 };
--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -3,9 +3,9 @@ const Nav = () => {
     tagNav.className = "header__nav nav"
     tagNav.innerHTML= `
     <ul class="nav__list">
-    <li class= "header__item"><a class="nav__link"href="#">Blog</a></li>
-    <li class= "header__item"><a class="nav__link"href="#">About Us</a></li>
-    <li class= "header__item"><a class="nav__link"href="#">Careers</a></li>
+    <li class= "nav__item"><a class="nav__link"href="#">Blog</a></li>
+    <li class= "nav__item"><a class="nav__link"href="#">About Us</a></li>
+    <li class= "nav__item"><a class="nav__link"href="#">Careers</a></li>
     </ul>
     `;
     return tagNav .outerHTML;
