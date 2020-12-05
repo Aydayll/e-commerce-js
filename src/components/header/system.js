dd810dc83d8e6d313cec55eb582019b5f485bc89
Aydayll <makenovaay23@gmail.com>
2020-12-06 03:05:12 +0600

    finish adding items in system.js

--- src/components/header/system.js
+++ src/components/header/system.js
@@ -3,7 +3,11 @@ const System = () => {
     const ul = document.createElement('ul');
 
 ul.innerHTML = `
-<li><a href="#"></a></li>
+<li><a href="#"></a>I am account</li>
 <li><a href="#"></a></li>
 `;
+div.append(ul);
+
+return div.outerHTML;
 };
+export default System();
