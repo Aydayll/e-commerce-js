f541578c0d6b8d4a13948a108b213c5b4f7966f0
Aydayll <makenovaay23@gmail.com>
2020-12-09 01:47:48 +0600

    import the hero to home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
 import Header from '../components/header/index.js';
+import Hero from '../components/hero/index.js';
 
 const Home = () => {
 const homePage = document.createElement('main');
 
-homePage.innerHTML = `${Header()}`;
+homePage.innerHTML = `
+${Header()}
+${Hero()};
+`;
   return homePage;
 };
 Home();
