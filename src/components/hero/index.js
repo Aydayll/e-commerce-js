4c2c543787b415855802da4caf202e571d16f3cd
Aydayll <makenovaay23@gmail.com>
2020-12-09 02:41:00 +0600

    add the aside to sidebar and import it

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,9 +1,11 @@
+import Sidebar from '../sidebar/index.js';
+
 const Hero = () => {
     const heroSection =  document.createElement ('section');
     heroSection.className = 'hero'
     heroSection.innerHTML = `
     <div class = "container">
-    
+      ${Sidebar()}
     </div>`
 
     return heroSection.outerHTML;
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,8 @@
+const Sidebar = () =>{
+   const aside = document.createElement('aside');
+
+   aside.className = 'hero__sidebar';
+   aside.innerHTML = 'xdrftyghunjk';
+   return aside.outerHTML
+};
+export default Sidebar();
