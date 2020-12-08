bdad572aef5ce889aa2449a59cdb5c06a0cb33bc
Aydayll <makenovaay23@gmail.com>
2020-12-09 03:35:35 +0600

    create the style for sidebar and import it

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,11 @@
-const Sidebar = () => {
+const Sidebar = (title, listItems, buttonText) => {
    const aside = document.createElement('aside');
 
    aside.className = `hero__sidebar sidebar col-3`;
 
-   aside.innerHTML = 'xdrftyghdxfchjbknmlnjk';
+   aside.innerHTML = `
+   <h3 class="sidebar__title">${title}</h3>
+		<ul class="sidebar__list">`;
    return aside.outerHTML;
 };
 export default Sidebar();
