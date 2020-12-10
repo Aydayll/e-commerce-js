d9fc1ddd2b63ba32025386fb15f2c2e6543bb875
Aydayll <makenovaay23@gmail.com>
2020-12-10 21:28:38 +0600

    started to style the banner

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -11,7 +11,7 @@ const Hero = () => {
 	<div class="container">
 		<div class="row hero__row">
 			${Sidebar(titleOfSidebar, listItemsOfSidebar, btnTextOfSidebar)}
-			<div class="hero__content row col-9">${Banner}${Banner}</div>
+			<div class="hero__content  banner__row row col-9">${Banner}${Banner}</div>
 		</div>
 	</div>
 	`;
