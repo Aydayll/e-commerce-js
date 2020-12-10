f45ce45208b39a2abd1ccc7ec584493661b01035
Aydayll <makenovaay23@gmail.com>
2020-12-10 21:23:25 +0600

    add the img for banner and items for this section

--- src/components/hero/banner.js
+++ src/components/hero/banner.js
@@ -5,15 +5,22 @@ const Banner = () => {
   const a = document.createElement('a');
   const img = document.createElement('img');
 
-  div.className = 'col-6';
+  div.className = 'col-6 banner';
   h6.className = 'banner__suptitle';
   h3.className = 'banner__title';
   a.className = 'banner__link';
   img.className = 'banner__image';
+  img.src = `https://www.flavcity.com/wp-content/uploads/2018/05/healthy-meal-prep-recipes.jpg`;
 
-  h3.innerText = `ertfyjgkuhjlk`;
+  h6.innerText = `Meal Prep Recipes`;
+  h3.innerText = `Tomato foccacia`;
+  a.innerHTML = `Read Recipes`;
 
+
+  div.append(h6);
   div.append(h3);
+  div.append(a);
+  div.append(img);
   return div.outerHTML;
 };
 export default Banner();
