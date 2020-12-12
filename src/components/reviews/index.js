4be89518752af7682f3e220144505a57ffd3b29b
Aydayll <makenovaay23@gmail.com>
2020-12-13 04:58:20 +0600

    style the items and change avatat

--- src/components/reviews/index.js
+++ src/components/reviews/index.js
@@ -4,7 +4,8 @@ const Reviews = () => {
     const p = document.createElement('p');
     const h2 = document.createElement('h2');
     const h3 = document.createElement('h3');
-    const img = document.createElement('img')
+    const img = document.createElement('img');
+    const column = document.createElement('div');
 
     section.className = 'reviews';
     h2.className = 'reviews__title';
@@ -12,21 +13,26 @@ const Reviews = () => {
     img.className = 'reviews__image';
     p.className = 'reviews__description';
     div.className = 'reviews__item';
+    column.className = 'col-4'
 
-    img.src = './src/images/avatar.jpg';
+    img.src = './src/images/avatar.png';
     img.alt = 'avatar';
 
     p.innerHTML = '“ This is an super space for your customers qoute. Don’t worry it works smooth as pie. You will get all what you need by writiing a text here “'
     h3.innerHTML = ' Name and Surname'
 
-    div.append(p)
-    div.append(h3)
-    div.append(img)
+    div.append(p);
+    div.append(h3);
+    div.append(img);
+    column.append(div);
     
   section.innerHTML = `
   ${h2.outerHTML}
   <div class = 'row reviews__row'>
-    ${div.outerHTML}
+    ${column.outerHTML}
+    ${column.outerHTML}
+    ${column.outerHTML}
+    ${column.outerHTML}
   </div>
   `;
  return section.outerHTML;
--- src/pages/home.js
+++ src/pages/home.js
@@ -7,6 +7,7 @@ const Home = () => {
   homePage.innerHTML = `
   ${Hero}
   ${Reviews()}
+  
 	`;
 
   return homePage;
